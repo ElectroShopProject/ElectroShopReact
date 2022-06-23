8344588b21fc416ea6c9b65dbeda3a738b99718e
Tomasz Kądziołka <tkadziolka@MacBook-Pro-Tomasz.local>
2022-06-23 18:17:19 +0200

    Configured dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '^react-native$': 'react-native-web',
+        },
+      },
+      'react-native-web',
+    ],
+  ],
 };
