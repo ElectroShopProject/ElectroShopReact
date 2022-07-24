4d6d9c92094838a0c24abf90fa7cf3556e1ef723
Tomasz Kądziołka <tkadziolka@MacBook-Pro-Tomasz.local>
2022-07-24 18:18:15 +0200

    Migrated login screen to normal styles

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -50,53 +50,94 @@ export function LoginScreen({navigation}) {
     // })
 
     return (
-        <Fullscreen>
-            <SafeAreaView>
+        <View style={{maxWidth: 1024, alignSelf: 'center', paddingHorizontal: 16}}>
+            <View style={{height: Dimensions.get('window').height, justifyContent: 'space-around'}}>
                 <Center>
-                    <PlatformWidth>
-                        <Center>
-                            <View style={{backgroundColor: 'grey', height: 100, width: 100}}/>
-                        </Center>
-                        <Center>
-                            <Image
-                                source={require('../assets/images/logo.png')}
-                                style={{
-                                    width: 300,
-                                    aspectRatio: 4.8,
-                                    resizeMode: 'contain',
-                                }}
-                            />
-                        </Center>
-                        <Text>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-                            has
-                            been
-                            the
-                            industry's standard dummy text ever since the 1500s, when an unknown printer took a
-                            galley
-                            of
-                            type
-                            and scrambled it to make a type specimen book. It has survived not only five centuries,
-                            but
-                            also
-                            the
-                            leap into electronic typesetting, remaining essentially unchanged. It was popularised in
-                            the
-                            1960s
-                            with the release of Letraset sheets containing Lorem Ipsum passages, and more recently
-                            with
-                            desktop
-                            publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                        </Text>
-                        <TextInput
-                            defaultValue={text}
-                            helperText={'Enter your login'}
-                            onChangeText={newText => setText(newText)}/>
-                        <Button title={'Done'} onPress={() => postLogin()}/>
-                    </PlatformWidth>
+                    <Image
+                        source={require('../assets/images/logo.png')}
+                        style={{
+                            width: 300,
+                            aspectRatio: 4.8,
+                            resizeMode: 'contain',
+                        }}
+                    />
                 </Center>
-            </SafeAreaView>
-        </Fullscreen>
+                <Text>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+                    has
+                    been
+                    the
+                    industry's standard dummy text ever since the 1500s, when an unknown printer took a
+                    galley
+                    of
+                    type
+                    and scrambled it to make a type specimen book. It has survived not only five centuries,
+                    but
+                    also
+                    the
+                    leap into electronic typesetting, remaining essentially unchanged. It was popularised in
+                    the
+                    1960s
+                    with the release of Letraset sheets containing Lorem Ipsum passages, and more recently
+                    with
+                    desktop
+                    publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </Text>
+                <TextInput
+                    defaultValue={text}
+                    helperText={'Enter your login'}
+                    onChangeText={newText => setText(newText)}/>
+                <Button title={'Done'} onPress={() => postLogin()}/>
+            </View>
+        </View>
+
+        // <Fullscreen>
+        //     <SafeAreaView>
+        //         <Center>
+        //             <PlatformWidth>
+        //                 <Center>
+        //                     <View style={{backgroundColor: 'grey', height: 100, width: 100}}/>
+        //                 </Center>
+        //                 <Center>
+        //                     <Image
+        //                         source={require('../assets/images/logo.png')}
+        //                         style={{
+        //                             width: 300,
+        //                             aspectRatio: 4.8,
+        //                             resizeMode: 'contain',
+        //                         }}
+        //                     />
+        //                 </Center>
+        //                 <Text>
+        //                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+        //                     has
+        //                     been
+        //                     the
+        //                     industry's standard dummy text ever since the 1500s, when an unknown printer took a
+        //                     galley
+        //                     of
+        //                     type
+        //                     and scrambled it to make a type specimen book. It has survived not only five centuries,
+        //                     but
+        //                     also
+        //                     the
+        //                     leap into electronic typesetting, remaining essentially unchanged. It was popularised in
+        //                     the
+        //                     1960s
+        //                     with the release of Letraset sheets containing Lorem Ipsum passages, and more recently
+        //                     with
+        //                     desktop
+        //                     publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        //                 </Text>
+        //                 <TextInput
+        //                     defaultValue={text}
+        //                     helperText={'Enter your login'}
+        //                     onChangeText={newText => setText(newText)}/>
+        //                 <Button title={'Done'} onPress={() => postLogin()}/>
+        //             </PlatformWidth>
+        //         </Center>
+        //     </SafeAreaView>
+        // </Fullscreen>
     )
 }
 
